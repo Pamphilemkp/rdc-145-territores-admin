d48d7b3ea6345d6aab83d64fccc67c4720e820cb
pamphilemkp <pamphilemkp@gmail.com>
2023-07-07 18:20:45 +0300

    trying to fix the migrate issue on the production

--- db/migrate/20230628205809_add_first_name_to_participant.rb
+++ db/migrate/20230628205809_add_first_name_to_participant.rb
@@ -1,5 +1,5 @@
 class AddFirstNameToParticipant < ActiveRecord::Migration[7.0]
   def change
-    add_column :participants, :first_name, :string
+    #add_column :participants, :first_name, :string
   end
 end
