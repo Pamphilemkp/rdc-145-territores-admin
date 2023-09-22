8c87ef1d3bdf3e47ababe367ba6bb2367952c7f6
pamphilemkp <pamphilemkp@gmail.com>
2023-09-22 11:36:15 +0300

    delete controllers devise registrations routes

--- app/controllers/devise/registrations_controller.rb
+++
@@ -1,9 +0,0 @@
-class RegistrationsController < Devise::RegistrationsController
-  # before_action :redirect_to_login, only: [:new, :create]
-
-  # private
-
-  # def redirect_to_login
-  #   redirect_to new_devise_user_session_path
-  # end
-end
