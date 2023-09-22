8ad541497b16e15adb6786bf35763d4568284f9f
pamphilemkp <pamphilemkp@gmail.com>
2023-09-22 11:24:08 +0300

    slightly edit the registration controller

--- app/controllers/devise/registrations_controller.rb
+++ app/controllers/devise/registrations_controller.rb
@@ -1,11 +1,9 @@
-# app/controllers/devise/registrations_controller.rb
-class Devise::RegistrationsController < Devise::RegistrationsController
-    # before_action :redirect_to_login, only: [:new, :create]
-  
-    # private
-  
-    # def redirect_to_login
-    #   redirect_to new_devise_user_session_path
-    # end
+class RegistrationsController < Devise::RegistrationsController
+  before_action :redirect_to_login, only: [:new, :create]
+
+  private
+
+  def redirect_to_login
+    redirect_to new_devise_user_session_path
   end
-  
+end
