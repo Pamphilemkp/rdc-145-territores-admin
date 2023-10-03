a4ff39ccd311bd1ae5223325394c8dcb6c2fc738
root <root@DESKTOP-1TGUH6U>
2023-10-03 20:39:47 +0500

    Add soultion for 401 error

--- app/controllers/ambassadors_controller.rb
+++ app/controllers/ambassadors_controller.rb
@@ -1,6 +1,5 @@
 class AmbassadorsController < ApplicationController
   before_action :set_ambassador, only: %i[ show edit update destroy ]
-  before_action :authenticate_devise_user!
   # GET /ambassadors or /ambassadors.json
   def index
     @ambassadors = Ambassador.all
