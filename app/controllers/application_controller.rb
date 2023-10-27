35bc8b237612ba8df75def3ba99c96126033a057
pamphilemkp <pamphilemkp@gmail.com>
2023-10-27 17:17:58 +0300

    add pundit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :custom_authentication
   include Pundit::Authorization
+  rescue_from Pundit:NotAuthorizedError, with: :user_not_authorized 
 
   private
 
@@ -10,6 +11,11 @@ class ApplicationController < ActionController::Base
     else
       authenticate_devise_user!
     end
+  end
+
+  def  user_not_authorized
+    flash[:alert] = 'You are not authorized to perform this action.'
+    redirect_to root_path
   end
 
   def after_sign_in_path_for(resource)
